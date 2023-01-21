fb5e453e7f67c10482e050be06002480d1df6260
Michael Chekole <63680542+micted@users.noreply.github.com>
2023-01-22 00:13:57 +0100

    Update backend.tf

--- DevOps_Project_3/Terraform/backend.tf
+++ DevOps_Project_3/Terraform/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
     key = "eks/cluster_name/statefile"
-    bucket = "testrdsdeme"
-    key = "eks/ngg_cluster_name/statefile"
+    bucket = "testrdsdeme"    
     region = "us-east-1"
   }
 } 
